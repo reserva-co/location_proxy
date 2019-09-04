08d8f2b7e01082a01ee8a6a659c1f2b8c26f50e0
Ingrid <ingridsu@yahoo.com>
2019-09-04 08:21:24 -0700

    proxy

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('/listing/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
